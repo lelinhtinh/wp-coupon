b7f5d87f2a516f664376cb969c8323106d518798
Lê Thành <lhthanh.pro@gmail.com>
2022-11-29 16:02:56 +0700

    feat: add get_request_parameter

--- includes/helpers.php
+++ includes/helpers.php
@@ -52,3 +52,16 @@ function endsWith($haystack, $needle)
     }
     return substr($haystack, -$length) === $needle;
 }
+
+function get_request_parameter($key, $default = '')
+{
+    if (!isset($_REQUEST[$key]) || empty($_REQUEST[$key])) {
+        return $default;
+    }
+    if (is_array($_REQUEST[$key])) {
+        return array_map(function ($v) {
+            return strip_tags((string) wp_unslash($v));
+        }, $_REQUEST[$key]);
+    }
+    return strip_tags((string) wp_unslash($_REQUEST[$key]));
+}
